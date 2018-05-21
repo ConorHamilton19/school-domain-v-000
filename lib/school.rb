02d73620944a131d2ab583b9fddb8618063b3bbe
Conor Hamilton <conhamil19@gmail.com>
2018-05-21 05:58:57 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  attr_reader :school
+  
+  ROSTER = {}
+  
+  def initialize(school) 
+    @school = school 
+  end
+  
+  def roster 
+    puts ROSTER 
+  end
+  
+  def add_student(name, grade)
+    ROSTER << {grade => [name]} 
+  end
+    
+end
